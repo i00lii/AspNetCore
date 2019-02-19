1154d2d07b3e05ce4959320f52c8fc1e7a63f5c3
Pavel Krymets <pavel@krymets.com>
2019-02-19 10:05:51 -0800

    wip

--- eng/scripts/GenerateReferenceAssemblies.ps1
+++ eng/scripts/GenerateReferenceAssemblies.ps1
@@ -5,4 +5,4 @@ $ErrorActionPreference = 'stop'
 
 $repoRoot = Resolve-Path "$PSScriptRoot/../.."
 
-& "$repoRoot\build.ps1" -ci:$ci -BuildManaged /t:GenerateReferenceSources
+& "$repoRoot\build.ps1" -ci:$ci -BuildManaged -NoBuildNodeJS /t:GenerateReferenceSources
